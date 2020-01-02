10ce3e1665677752beb51d9a354178017e9bbed8
Robert Garrison <robert.garrison@unityfisolutions.com>
2020-01-02 06:54:27 -0500

    adding region

--- ADONetHelper.Core/IDbParameterMapper.cs
+++ ADONetHelper.Core/IDbParameterMapper.cs
@@ -32,11 +32,13 @@ namespace ADONetHelper.Core
     /// </summary>
     public interface IDbParameterMapper
     {
+        #region Utility Methods
         /// <summary>
         /// Gets the <see cref="DbType"/> associated with the passed in <paramref name="parameterValue"/>
         /// </summary>
         /// <param name="parameterValue">The .NET value that will be mapped to a providers native data type</param>
         /// <returns>Returns a <see cref="DbType"/> value that describes the RDBMS type of passed in <paramref name="parameterValue"/></returns>
         DbType GetDbType(object parameterValue);
+        #endregion
     }
 }
