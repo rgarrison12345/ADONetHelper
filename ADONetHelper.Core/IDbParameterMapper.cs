538a77a80f08567e7cf30084ca2e213ce7c9e680
Robert Garrison <robert.garrison@unityfisolutions.com>
2020-01-01 20:59:55 -0500

    Intellisesnse updates

--- ADONetHelper.Core/IDbParameterMapper.cs
+++ ADONetHelper.Core/IDbParameterMapper.cs
@@ -5,15 +5,15 @@ using System.Data;
 namespace ADONetHelper.Core
 {
     /// <summary>
-    /// 
+    /// Contract class the defines the behavior of a DbParameter mapper class
     /// </summary>
     public interface IDbParameterMapper
     {
         /// <summary>
-        /// Gets the type of the database.
+        /// Gets the <see cref="DbType"/> associated with the passed in <paramref name="parameterValue"/>
         /// </summary>
-        /// <param name="value">The value.</param>
-        /// <returns></returns>
-        DbType GetDbType(object value);
+        /// <param name="parameterValue">The .NET value that will be mapped to a providers native data type</param>
+        /// <returns>Returns a <see cref="DbType"/> value that describes the RDBMS type of passed in <paramref name="parameterValue"/></returns>
+        DbType GetDbType(object parameterValue);
     }
 }
