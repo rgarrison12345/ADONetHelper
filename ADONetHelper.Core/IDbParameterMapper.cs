e838a47f11ac5627d8e6a64e792c5afddd96bdfc
Robert Garrison <robert.garrison@unityfisolutions.com>
2020-01-01 19:58:16 -0500

    Version increase

---
+++ ADONetHelper.Core/IDbParameterMapper.cs
@@ -0,0 +1,19 @@
+﻿#region Using Statements
+using System.Data;
+#endregion
+
+namespace ADONetHelper.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IDbParameterMapper
+    {
+        /// <summary>
+        /// Gets the type of the database.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        DbType GetDbType(object value);
+    }
+}
